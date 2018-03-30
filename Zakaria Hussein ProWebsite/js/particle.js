cba2a138186cff956426a66c09846d97300102e6
Zakaria Hussein <xzak01@gmail.com>
2018-03-29 17:25:42 -0700

    Revert "Initial Commit"
    This reverts commit 0f20375777b4f1f1a6f2e4bf757c3a2c2a83bb20.

--- Zakaria Hussein ProWebsite/js/particle.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles_bg'), {
-    dotColor: '#000',
-    lineColor: '#000'
-  });
-}, false);
