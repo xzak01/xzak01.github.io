f31c5c1ed9293b4d0ed58dc380a3cdf4314da982
xzak01 <37785375+xzak01@users.noreply.github.com>
2018-09-11 15:14:04 -0700

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,17 @@
+﻿(function ($) {
+
+    "use strict";
+
+        // jquery backstretch slideshow
+        $("#slider").backstretch([
+          "img/slider1.jpg",
+          "img/slider2.jpg",
+		  "img/slider3.jpg"
+          ], {duration: 2000, fade: 750});
+       
+})(jQuery);
+
+
+
+
+
